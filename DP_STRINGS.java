89841ee2dc86d51f887cef84137f69527b52f425
Suhani-Singhal2131 <suhanisinghal2131@gmail.com>
2025-02-18 21:59:57 +0530

    modified on day 5

--- DP_STRINGS.java
+++ DP_STRINGS.java
@@ -53,7 +53,7 @@ public class DP_STRINGS {
    }
    
    //printing longest common subsequence
-   public static void helper(String s,String t,ArrayList<String>ans,int n,int m,int[][]dp){
+   public static void helper(String s,String t,int n,int m,int[][]dp){
     int len=dp[n][m];
     int i=n;
     int j=m;
@@ -74,7 +74,7 @@ public class DP_STRINGS {
         }
         else j--;
     }
-    ans.add(str.toString());
+    System.out.println(str.toString());
 }
    
    public static void L_C_substring_1(String s1,String s2,int[][]dp){
@@ -111,7 +111,7 @@ public class DP_STRINGS {
     return ans;
 }
    
- public static int longestPalindromeSubseq(String s) {
+   public static int longestPalindromeSubseq(String s) {
     int n=s.length();
     StringBuilder str=new StringBuilder(s);
     str.reverse();
@@ -132,10 +132,150 @@ public class DP_STRINGS {
     return dp[n][n];
     
 }
-public static void main(String[] args) {
-     String s1="abcdgh";
+ 
+   //In finding longest palindromic subsequence we take the original string as s1 and 
+   //revese of s1 as s2 then just find LCS;
+
+   //In min insertion steps to make string palindrome
+   //s1.length()-LCS(s1,rev(s1))
+
+ //SHORTEST COMMON SUPERSEQUENCE
+ public static void S_C_S(String s1,String s2){
+    int n=s1.length();
+    int m=s2.length();
+    int[][]dp=new int[n+1][m+1];
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s1.charAt(i-1)==s2.charAt(j-1)){
+                dp[i][j]=1+dp[i-1][j-1];
+            }
+            else{
+                dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+
+    int i=n;
+    int j=m;
+    StringBuilder s= new StringBuilder();
+    while(i>0 && j>0){
+        if(s1.charAt(i-1)==s2.charAt(j-1)){
+            s.append(s1.charAt(i-1));
+            i--;
+            j--;
+        }
+        else if(dp[i-1][j]>dp[i][j-1]){
+            s.append(s1.charAt(i-1));
+            i--;
+        }
+        else{
+            s.append(s2.charAt(j-1));
+            j--;
+        }
+    }
+    while(i>0){
+        s.append(s1.charAt(i-1));
+        i--; 
+    }
+    while(j>0){
+        s.append(s2.charAt(j-1));
+        j--;
+    }
+    System.out.println(s.reverse());
+ }
+
+ //DISTINCT SUBSEQUENCES
+ public static int distinct_subsequences(String s1,String s2,int i,int j){
+    if(j==0)return 1;
+    if(i==0)return 0;
+
+    if(s1.charAt(i-1)==s2.charAt(j-1)){
+        return distinct_subsequences(s1, s2, i-1, j-1)+distinct_subsequences(s1, s2, i-1, j);
+    }
+    return distinct_subsequences(s1, s2, i-1, j);
+ }
+ public static int distinct_subsequences_2(String s1,String s2,int i,int j,int[][]dp){
+    if(j==0)return 1;
+    if(i==0)return 0;
+    if(dp[i][j]!=-1)return dp[i][j];
+    if(s1.charAt(i-1)==s2.charAt(j-1)){
+        return dp[i][j]=distinct_subsequences(s1, s2, i-1, j-1)+distinct_subsequences(s1, s2, i-1, j);
+    }
+    return dp[i][j]=distinct_subsequences(s1, s2, i-1, j);
+ }
+ public static int distinct_subsequences_3(String s1,String s2,int n,int m,int[][]dp){
+    for(int i=0;i<=n;i++)dp[i][0]=1;
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s1.charAt(i-1)==s2.charAt(j-1)){
+                dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+            }
+            else  dp[i][j]=dp[i-1][j];
+        }
+    }
+    return dp[n][m];
+ }
+ public static int distinct_subsequences_4(String s1,String s2,int n,int m){
+    int[]prev=new int[m+1];
+    int[]curr=new int[m+1];
+    prev[0]=curr[0]=1;
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s1.charAt(i-1)==s2.charAt(j-1)){
+                curr[j]=prev[j-1]+prev[j];
+            }
+            else  curr[j]=prev[j];
+        }
+        prev=curr;
+    }
+    return prev[m];
+ }
+
+
+ public static int edit_distance(String s1,String s2,int i,int j){
+    if(i<0)return j+1;
+    if(j<0)return i+1;
+
+    if(s1.charAt(i)==s2.charAt(j)){
+        return edit_distance(s1, s2, i-1, j-1);
+    }
+    return 1+Math.min(edit_distance(s1, s2, i-1, j-1),Math.min(edit_distance(s1, s2, i-1, j),edit_distance(s1, s2, i, j-1))) ;
+ }
+ public static int edit_distance_2(String s1,String s2,int i,int j,int[][]dp){
+    if(i<0)return j+1;
+    if(j<0)return i+1;
+    if(dp[i][j]!=-1)return dp[i][j];
+    if(s1.charAt(i)==s2.charAt(j)){
+        return dp[i][j]=edit_distance_2(s1, s2, i-1, j-1,dp);
+    }
+    return dp[i][j]=1+Math.min(edit_distance_2(s1, s2, i-1, j-1,dp),Math.min(edit_distance_2(s1, s2, i-1, j,dp),edit_distance_2(s1, s2, i, j-1,dp))) ;
+ }
+ public static int edit_distance_3(String s1,String s2,int n,int m,int[][]dp){
+    for (int i = 0; i <= n; i++) {
+        dp[i][0] = i;
+    }
+    for (int j = 0; j <= m; j++) {
+        dp[0][j] = j;
+    }
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s1.charAt(i-1)==s2.charAt(j-1)){
+                dp[i][j]=dp[i-1][j-1];
+            }
+            else dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1])) ;
+        }
+    }
+    return dp[n][m];
+ }
+
+ public static void main(String[] args) {
+     String s1="horse";
      int n=s1.length();
-     String s2="acdghr";
+     String s2="ros";
      int m=s2.length();
      int[][]dp=new int[n+1][m+1];
 
@@ -143,16 +283,14 @@ public static void main(String[] args) {
     //     Arrays.fill(row,-1);
     //  }
 
-    // LCS_3(s1, s2,dp);
+     System.out.println(edit_distance_3(s1, s2, n, m,dp));
     //  for(int[]row:dp){
     //     for(int val: row){
     //         System.out.print(val+" ");
     //     }
     //     System.out.println();
     //  }
-
-    // helper(s1,s2,ans,n,m,dp);
-    System.out.println(L_C_substring_2(s1,s2,n,m));
+   
     
    
 
